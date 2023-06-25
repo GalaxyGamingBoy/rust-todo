a34584c1ff66f670c6d900bd7466f88f0c24c438
GalaxyGamingBoy <xrteach@gmail.com>
2023-06-25 18:13:26 +0000

    fix: Made todo name primary

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
         .subcommand_required(true)
         .subcommand(
             clap::Command::new("new").about("Creates a ToDo").args([
-                clap::arg!(-n --name <TODO_NAME> "The name of the todo that will be created.")
+                clap::arg!([TODO_NAME] "The name of the todo that will be created.")
                     .required(true).value_parser(clap::builder::NonEmptyStringValueParser::new()),
                 clap::arg!(-d --desc <TODO_DESCRIPTION> "The description of the todo that will be created.").default_value(""),
                 clap::arg!(-m --mark <TODO_MARKED> "The completion status if the todo that will be created.").value_parser(["yes", "no"])
