4d41d741e6ea051918a1e8928217ed47b9bb5f7f
Marios <xrteach@gmail.com>
2023-07-10 12:29:28 +0300

    feat: Mark use mark arguement

--- src/todo/commands/mark/mod.rs
+++ src/todo/commands/mark/mod.rs
@@ -1,6 +1,6 @@
 use clap::parser::ArgMatches;
 
-use crate::todo::{Todo, get_todo_index};
+use crate::todo::{Todo, get_todo_index, string_to_bool};
 
 
 pub fn mark_todo(args: &ArgMatches) {
@@ -8,7 +8,12 @@ pub fn mark_todo(args: &ArgMatches) {
     
     let todo_index = get_todo_index(args);
     let mut todo = Todo::load_from(todo_index);
-    todo.toggle_marked();
+
+    match args.get_one::<String>("mark") {
+        Some(e) => todo.change_marked(string_to_bool(e.to_owned())),
+        None => todo.toggle_marked()
+    }
+    
     todo.save();
 
     log::info!("Changed marked value to todo with index: {}", todo_index);
